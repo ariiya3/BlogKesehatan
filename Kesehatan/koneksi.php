1afba6e16e3f2b7f6bade0a1072a59c41436617b
Jafar001 <jsodiq88@gmail.com>
2018-05-21 06:05:39 +0700

    add undermaintenance

---
+++ Kesehatan/koneksi.php
@@ -0,0 +1,25 @@
+<?php
+  date_default_timezone_set('Asia/Jakarta');
+  session_start();
+
+  $server = 'localhost';
+  $user = 'root';
+  $pass = '';
+  $db = 'tb_artikel';
+
+  $con = mysqli_connect($server,$user,$pass,$db);
+
+  if (mysqli_connect_errno()) {
+    echo mysqli_connect_error();
+  }
+
+  function base_url($url = null){
+    $base_url = 'http://localhost/gabut/prakt/Kesehatan';
+    if ($url != null) {
+      return $base_url.'/'.$url;
+    }
+    else {
+      return $base_url;
+    }
+  }
+ ?>
